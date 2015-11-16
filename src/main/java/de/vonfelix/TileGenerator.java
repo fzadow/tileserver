0501cc998005a1bee543577899d7d599e255ad4f
fzadow <felix@von-zadow.de>
2015-11-16 19:29:44 +0100

    TileGenerator: support composite tiles

--- src/main/java/de/vonfelix/TileGenerator.java
+++ src/main/java/de/vonfelix/TileGenerator.java
@@ -19,8 +19,29 @@ import ch.systemsx.cisd.base.mdarray.MDShortArray;
 public class TileGenerator {
 	
 	int[] colormap;
+	int[] grayMap;
 
 	public TileGenerator() {
+	}
+	
+	private int[] getGrayMap() {
+		if( grayMap == null ) {
+			int limit = 2000;
+			double exp = 1;
+			int target = 256;
+			
+			grayMap = new int[65536];
+			for ( int i = 0; i < 65536; ++i ) {
+				if( i < limit ) {
+					int c = (int)( Math.pow( ((double)i/limit), (double)exp ) * target );
+					grayMap[ i ] = c;
+				} else {
+					// mark overexposed pixels;
+					grayMap[ i ] = 0b11111111;
+				}
+			}
+		}
+		return grayMap;
 	}
 	
 	private int[] getColormap() {
@@ -41,17 +62,17 @@ public class TileGenerator {
 				}
 				else {
 					// mark overexposed pixels
-					colormap[i] = 0b111111110000000011111111;
+					colormap[i] = ChannelColor.BLUE;
 				}
 			}
 			
 			// for debugging
 			if( Boolean.parseBoolean( Tileserver.getProperty("debug") ) ) {
 				System.out.println( "using debug colormap" );
-				colormap[40000] = 0b000000001111111100000000; // green
-				colormap[50000] = 0b111111110000000000000000; // red
-				colormap[65535] = 0b000000001111111111111111; // cyan
-				colormap[30000] = 0b001111110011111100111111; // grey
+				colormap[40000] = ChannelColor.GREEN;
+				colormap[50000] = ChannelColor.RED;
+				colormap[65535] = ChannelColor.CYAN;
+				colormap[30000] = ChannelColor.GRAYS;
 			}
 			
 			System.out.println( " ... done (" + ( System.nanoTime() - startTime ) / 1000000 + "ms)" );
@@ -117,53 +138,47 @@ public class TileGenerator {
 	}
 	
 
-	// TODO Work in progress
 	public BufferedImage getTile( CompositeStack stack, int scaleLevel, TileCoordinates coordinates ) throws Exception {
 
+		System.out.println( "getting composite tile" );
+		
 		final boolean debug = Boolean.parseBoolean( Tileserver.getProperty("debug") );
 		final boolean debug_tile_overlap = debug && Boolean.parseBoolean( Tileserver.getProperty("debug_tile_overlap") );
 		final boolean debug_tile_bounds = debug && Boolean.parseBoolean( Tileserver.getProperty("debug_tile_bounds") );
 		
 		int size = coordinates.getSize();
-		
+
+		// create square rgb array of width 'size'
+		int[] rgb = new int[ size * size ];
+
+		System.out.println( "TileGenerator: Generating composite tile ... " );
+
 		// get data for all channels
 		for( Stack channel : stack.getChannels().values() ) {
 			MDShortArray data = channel.getBlock( scaleLevel, size, coordinates.getZ(), coordinates.getX(), coordinates.getY() );
 			int data_width = data.dimensions()[2];
 			int data_height = data.dimensions()[1];
-			System.out.print( "TileGenerator: Generating composite tile ... " );
 			
 			short[] flatdata = data.getAsFlatArray();
 			
-			// create square rgb array of width 'size'
-			int[] rgb = new int[ size * size ];
+			System.out.println( "  Channel " + channel.getName() + ": " + stack.getColor( channel.getName() ) );
 			
 			for(int y = 0; y < size; ++y) {
 				for(int x= 0; x < size; ++x ) {
-
-					// fill overlap with black (or grey if in debug mode)
-					short fillpixel = (short) (debug_tile_overlap ? 30000 : 0 ) ; 
-					
-					short pixel = x >= data_width || y >= data_height ? fillpixel : flatdata[ data_width*y + x ];
-
-					if ( debug_tile_bounds ) {
-						if( ( x % 64 == 0 && y % 64 == 0 ) || ( (x-1) % 64 == 0 && y % 64 == 0 ) || ( x % 64 == 0 && (y-1) % 64 == 0 ) || ( (x+1) % 64 == 0 && y % 64 == 0 ) || ( x % 64 == 0 && (y+1) % 64 == 0 ) || ( (x-2) % 64 == 0 && y % 64 == 0 ) || ( x % 64 == 0 && (y-2) % 64 == 0 ) || ( (x+2) % 64 == 0 && y % 64 == 0 ) || ( x % 64 == 0 && (y+2) % 64 == 0 ) ) {
-							pixel = (short)30000;
-						}
-						if( ( x == 0 && y % 4 == 0 ) || ( y == 0 && x % 4 == 0 ) ) {
-							pixel = (short)40000;
-						}
-						if( ( x == size-1 && y % 4 == 0 ) || ( y == size-1 && x % 4 == 0 ) ) {
-							pixel = (short)50000;
-						}
-					}
-					
-					rgb[ size*y + x ] = getColormap()[pixel & 0xffff ];
+					// fill overlap with black
+					short pixel = x >= data_width || y >= data_height ? 0 : flatdata[ data_width*y + x ];
+					int grayValue = getGrayMap()[pixel & 0xffff ];
+					rgb[ size*y + x ] = rgb[ size*y + x ] | ( ( grayValue << 16 ) | (grayValue << 8 ) | grayValue ) & ChannelColor.getColor( stack.getColor( channel.getName() ) );
 				}
 			}
 		}
+		BufferedImage img = new BufferedImage(size, size, BufferedImage.TYPE_3BYTE_BGR);
 
-		return null;
+		img.setRGB(0, 0, size, size, rgb, 0, size);
+
+//		System.out.println( " done (" + ( System.nanoTime() - startTime ) / 1000000 + "ms)" );
+
+		return img;
 	}
 	
 //	public byte[] getTileAsByteArray( IStack stack, int scaleLevel, TileCoordinates coordinates ) throws Exception {
