94d45b1c4166badea2d56aa85b8d45770c2971ce
fzadow <felix@von-zadow.de>
2016-02-24 16:21:08 +0100

    hdf5YAML

--- src/main/java/de/vonfelix/tileserver/image/HDF5YAMLImage.java
+++ src/main/java/de/vonfelix/tileserver/image/HDF5YAMLImage.java
@@ -12,6 +12,7 @@ import org.yaml.snakeyaml.Yaml;
 
 import ch.systemsx.cisd.hdf5.HDF5Factory;
 import ch.systemsx.cisd.hdf5.IHDF5Reader;
+import de.vonfelix.tileserver.Adjustable.Adjustment;
 import de.vonfelix.tileserver.Color;
 import de.vonfelix.tileserver.Tileserver;
 import de.vonfelix.tileserver.stack.Channel;
@@ -46,108 +47,71 @@ public class HDF5YAMLImage extends AbstractImage {
 
 			ArrayList yamlstacks = (ArrayList) ( (HashMap) ( (HashMap) data ).get( "project" ) ).get( "stacks" );
 
+			Integer max = (Integer) ( (HashMap) ( (HashMap) data ).get( "project" ) ).get( "max" );
+
 			// TODO set valueLimit
 
-			// read stacks
-			stacks = loadStacks( yamlstacks );
+
+			// READ STACKS
+
+			for ( Object s : yamlstacks ) {
+				HashMap st = (HashMap) s;
+
+				// Composite Stack
+				if ( st.containsKey( "channels" ) ) {
+					String id = (String) st.get( "folder" );
+					String title = (String) st.get( "name" );
+					max = st.containsKey( "max" ) ? (Integer) st.get( "max" ) : max;
+
+					logger.trace( "  composite stack " + id + " (" + title + ")" );
+
+					CompositeStack cs = new CompositeStack( this, id, title );
+
+					ArrayList ch = (ArrayList) st.get( "channels" );
+					for ( Object c : ch ) {
+						HashMap cha = (HashMap) c;
+						String stack_id = (String) cha.get( "stack" );
+						String color = (String) cha.get( "color" );
+						max = cha.containsKey( "max" ) ? (Integer) cha.get( "max" ) : max;
+
+						logger.trace( "    " + stack_id + " : " + color );
+						HDF5Stack stack = (HDF5Stack) stacks.get( stack_id );
+						Channel channel = new Channel( stack, Color.valueOf( color.toUpperCase() ) );
+						if ( max != null )
+							channel.setAdjustment( Adjustment.MAX_VALUE, max );
+						cs.addChannel( channel );
+					}
+					stacks.put( cs.getId(), cs );
+				}
+				// normal Stack
+				else {
+					String id = (String) st.get( "folder" );
+					String title = (String) st.get( "name" );
+					String path = "stacks/";
+					max = st.containsKey( "max" ) ? (Integer) st.get( "max" ) : max;
+
+					logger.trace( "  stack " + path + "" + id + " (" + title + ")" );
+
+					IStack sta = new HDF5Stack( this, path, id, title );
+					if ( max != null ) {
+						sta.setValueLimit( max );
+					}
+					stacks.put( sta.getId(), sta );
+				}
+
+			}
+
+			if ( stacks.size() == 0 ) {
+				logger.warn( "No stacks found for image " + name );
+			}
+
 		} catch ( IOException e ) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	private HashMap<String, IStack> loadStacks( ArrayList yamlstacks ) {
 
-		for ( Object s : yamlstacks ) {
-			HashMap st = (HashMap) s;
-			System.out.println( st.get( "name" ) );
-
-			// Composite Stack
-			if ( st.containsKey( "channels" ) ) {
-				String id = (String) st.get( "folder" );
-				String title = (String) st.get( "name" );
-
-				logger.trace( "  composite stack " + id + " (" + title + ")" );
-
-				CompositeStack cs = new CompositeStack( this, id, title );
-
-				ArrayList ch = (ArrayList) st.get( "channels" );
-				for ( Object c : ch ) {
-					HashMap cha = (HashMap) c;
-					String stack_id = (String) cha.get( "stack" );
-					String color = (String) cha.get( "color" );
-
-					logger.trace( "    " + stack_id + " : " + color );
-					HDF5Stack stack = (HDF5Stack) stacks.get( stack_id );
-					Channel channel = new Channel( stack, Color.valueOf( color.toUpperCase() ) );
-					cs.addChannel( channel );
-				}
-				stacks.put( cs.getId(), cs );
-			}
-			// normal Stack
-			else {
-				String id = (String) st.get( "folder" );
-				String title = (String) st.get( "name" );
-				String path = "stacks/";
-
-				logger.trace( "  stack " + path + "" + id + " (" + title + ")" );
-
-				IStack sta = new HDF5Stack( this, path, id, title );
-				stacks.put( sta.getId(), sta );
-			}
-
-		}
-
-		//		if ( imageDesc != null ) {
-		//			// TODO sanity checks when loading channels (no dupes, only sane
-		//			// composites)
-		//			logger.trace( "reading stack info for " + name );
-		//			Element root = imageDesc.getRootElement();
-		//			Namespace ns = root.getNamespace();
-		//
-		//			// load all normal Stacks (children of <Stacks> in xml)
-		//			for ( Element s : root.getChild( "Stacks", ns ).getChildren() ) {
-		//				logger.trace( "  stack " + s.getChildText( "path", ns ) + "" + s.getChildText( "id", ns ) + " (" + s.getChildText( "title", ns ) + "), value limit: " + s.getChildText( "value_limit", ns ) );
-		//				// Stack st = new Stack( this, s.getChildText( "path", ns ),
-		//				// s.getChildText( "id", ns ) );
-		//
-		//				String id = s.getChildText( "id", ns );
-		//				String title = s.getChildText( "title", ns );
-		//				String path = s.getChildText( "path", ns );
-		//				IStack st = new HDF5Stack( this, path, id, title );
-		//
-		//				if ( s.getChildText( "value_limit", ns ) != null ) {
-		//					st.setValueLimit( Integer.parseInt( s.getChildText( "value_limit", ns ) ) );
-		//				}
-		//				stacks.put( st.getId(), st );
-		//			}
-		//
-		//			// load all composite Stacks (children of <CompositeStacks> in xml)
-		//			for ( Element s : root.getChild( "CompositeStacks", ns ).getChildren() ) {
-		//				logger.trace( "  composite stack " + s.getChildText( "id", ns ) + " (" + s.getChildText( "title", ns ) + ")" );
-		//				CompositeStack cs = new CompositeStack( this, s.getChildText( "id", ns ), s.getChildText( "title", ns ) );
-		//				for ( Element c : s.getChild( "Channels", ns ).getChildren() ) {
-		//					logger.trace( "    " + c.getChildText( "stack_id", ns ) + " : " + c.getChildText( "color", ns ) + ", value limit: " + c.getChildText( "value_limit", ns ) );
-		//					HDF5Stack stack = (HDF5Stack) stacks.get( c.getChildText( "stack_id", ns ) );
-		//					Channel channel = new Channel( stack, Color.valueOf( c.getChildText( "color", ns ).toUpperCase() ) );
-		//					if ( c.getChildText( "value_limit", ns ) != null ) {
-		//						channel.setValueLimit( Integer.parseInt( c.getChildText( "value_limit", ns ) ) );
-		//					}
-		//					cs.addChannel( channel );
-		//				}
-		//				stacks.put( cs.getId(), cs );
-		//			}
-		//		} else {
-		//			logger.warn( "No image description XML loaded!" );
-		//		}
-
-		if ( stacks.size() == 0 ) {
-			logger.warn( "No stacks found for image " + name );
-		}
-		
-		return stacks;
-	}
-	
 	public IHDF5Reader getReader() {
 		return reader;
 	}
