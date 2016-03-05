0e1cc7f128abac96ba3ff30ead301dffd92daa9d
fzadow <felix@von-zadow.de>
2016-03-05 01:28:59 +0100

    Add Header to allow Timing

--- src/main/java/de/vonfelix/tileserver/SimpleCORSFilter.java
+++ src/main/java/de/vonfelix/tileserver/SimpleCORSFilter.java
@@ -31,6 +31,10 @@ public class SimpleCORSFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+		// Allow timing measuring
+		response
+				.setHeader( "Timing-Allow-Origin", "*" );
 		chain.doFilter(req, response);
 	}
 
