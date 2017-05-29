91a7ad1572520d91b4ff45907b6611d5ec6db2fe
Felix von Zadow <felix@von-zadow.de>
2017-05-29 20:47:59 +0200

    YAML transformation: replace valuelimit with min/max

--- scripts/transform.py
+++ scripts/transform.py
@@ -11,18 +11,39 @@ data = yaml.load(stream)
 # Name extrahieren
 name = data['project']['name']
 
+# Get old maximum value
+max_val = data['project'].get('valuelimit')
+min_val = 0
+if max_val:
+    del data['project']['valuelimit']
+
 # neuen Namen eintragen
 data['project']['title'] = "DOT"
 del data['project']['name']
 
 # Stackgroups eintragen
 if not 'stackgroups' in data['project']:
-	data['project']['stackgroups'] = [{'title': name, 'min': 0, 'max':255}]
+        sg_data = {
+            'title': name
+        }
+        if max_val:
+            sg_data['min'] = min_val
+            sg_data['max'] = max_val
+
+	data['project']['stackgroups'] = [sg_data]
 
 # Dateiname als "path"
 for stack in data['project']['stacks']:
 	folder = stack['folder']
 	del stack['folder']
+
+        stack_max_val = stack.get('valuelimit')
+        stack_min_val = 0
+
+        if stack_max_val:
+            del stack['valuelimit']
+            stack['min'] = stack_min_val
+            stack['max'] = stack_max_val
 
 	stack['stackgroups'] = [{
 			'title': name,
