5715b4ac8d4ba3488e9810c1ef526bb940cce52e
Felix von Zadow <felix@von-zadow.de>
2017-05-29 21:39:34 +0200

    YAML transformation: replace tabs with spaces

--- scripts/transform.py
+++ scripts/transform.py
@@ -32,17 +32,17 @@ if not 'stackgroups' in data['project']:
             sg_data['min'] = min_val
             sg_data['max'] = max_val
 
-	data['project']['stackgroups'] = [sg_data]
+        data['project']['stackgroups'] = [sg_data]
 
 # Dateiname als "path"
 for stack in data['project']['stacks']:
-	folder = stack['folder']
-	del stack['folder']
+        folder = stack['folder']
+        del stack['folder']
 
         stack_max_val = stack.get('valuelimit')
         stack_min_val = 0
 
-        stack['title'] = '{} - {}'.format(name, stack['name'])
+        stack['title'] = u'{} - {}'.format(name, stack['name'])
         del stack['name']
 
         if stack_max_val:
@@ -55,16 +55,16 @@ for stack in data['project']['stacks']:
             stack['description'] = metadata.strip("'")
             del stack['metadata']
 
-	stack['stackgroups'] = [{
-			'title': name,
-			'relation': 'has_channel'}]
-	stack['mirrors'] = [{
-			'title': 'Dresden',
-			'folder': folder,
-			'tile_source_type': 9,
-			'path': os.path.splitext(filename)[0]}]
+        stack['stackgroups'] = [{
+                        'title': name,
+                        'relation': 'has_channel'}]
+        stack['mirrors'] = [{
+                        'title': 'Dresden',
+                        'folder': folder,
+                        'tile_source_type': 9,
+                        'path': os.path.splitext(filename)[0]}]
 
 
 
-print yaml.dump( data, default_flow_style=False)
+print(yaml.safe_dump( data, default_flow_style=False))
 
